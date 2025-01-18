7fa87703caa8bbc08effbd3db733833fc501a24a
Karim_Gamal <81161929+KarimGamal97@users.noreply.github.com>
2025-01-18 15:50:49 +0200

    Adding Files

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  // عند التمرير
+  $(window).on("scroll", function () {
+    // التحقق إذا كان التمرير قد تجاوز 50 بيكسل (يمكنك تغيير القيمة)
+    if ($(window).scrollTop() > 50) {
+      $(".navbar").addClass("fixed-nav");
+    } else {
+      $(".navbar").removeClass("fixed-nav");
+    }
+  });
+
+  $(".owl-suggestions").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    rtl: true,
+       navText: [
+      '<i class="fas fa-chevron-right"></i>', // Font Awesome right arrow
+        '<i class="fas fa-chevron-left"></i>' // Font Awesome left arrow
+    ],
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 2,
+        },
+        1000: {
+            items: 3,
+        },
+    },
+});
+
+});
