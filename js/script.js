6811e77042e2cd32dffca33ffcdecce4b06a71f9
Karim_Gamal <81161929+KarimGamal97@users.noreply.github.com>
2025-01-27 16:40:48 +0200

    swiper - sliders

--- js/script.js
+++ js/script.js
@@ -14,21 +14,63 @@ $(document).ready(function () {
     margin: 20,
     nav: true,
     rtl: true,
-       navText: [
+    navText: [
       '<i class="fas fa-chevron-right"></i>', // Font Awesome right arrow
-        '<i class="fas fa-chevron-left"></i>' // Font Awesome left arrow
+      '<i class="fas fa-chevron-left"></i>', // Font Awesome left arrow
     ],
     responsive: {
-        0: {
-            items: 1,
-        },
-        600: {
-            items: 2,
-        },
-        1000: {
-            items: 3,
-        },
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
     },
-});
+  });
 
+  const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: true,
+    slidesPerView: 3,
+    speed: 500,
+    loop: true,
+    cssMode: true,
+    navigation: true,
+    pagination: true,
+    scrollbar: true,
+    mouseWheel: true,
+    paginationClickable: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+    },
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 });
